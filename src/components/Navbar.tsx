2fb4c5269c2146e9a709df4ab97d8a00ef86a72b
Aman Thukral <thukral618@gmail.com>
2024-08-18 17:11:42 +0530

    Add Navbar styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,13 +2,17 @@
 
 import { ICart } from "@/models/cart.model";
 import { useCart } from "@/provider/CartProvider";
-import { ShoppingCart } from "lucide-react";
+import { AlignJustify, ShoppingCart, X } from "lucide-react";
 import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
 import { useState } from "react";
 
 const Navbar = () => {
   const { data: session } = useSession();
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleNavbar = () => {
+    setIsOpen(!isOpen);
+  };
 
   const { cart, localCart } = useCart() as {
     cart: ICart | undefined;
@@ -16,35 +20,86 @@ const Navbar = () => {
   };
 
   return (
-    <div className="flex justify-between mx-8 py-4">
-      <Link href={"/"}>Teevolution</Link>
-      <div className="flex w-[10%] justify-between">
-        <Link href={"/cart"} className="flex">
-          <ShoppingCart />
-          <p className="pl-1">Cart</p>
+    <header className="mx-8 py-4">
+      <div className="flex justify-between items-center flex-wrap">
+        <Link href={"/"}>Teevolution</Link>
+        <div className="hidden md:flex md:justify-between">
+          <Link href={"/cart"} className="flex mr-2">
+            <ShoppingCart />
+            <p className="pl-1">Cart</p>
 
-          <div className="p-1 h-5 w-5 bg-red-500 text-white rounded-[50%] flex justify-center items-center">
             {session
-              ? cart && cart.cartItems && cart.cartItems.length
-              : localCart.length}
+              ? cart &&
+                cart.cartItems &&
+                cart.cartItems.length > 0 && (
+                  <div className="p-1 h-5 w-5 bg-red-500 text-white rounded-[50%] flex justify-center items-center">
+                    {cart.cartItems.length}
+                  </div>
+                )
+              : localCart &&
+                localCart.length > 0 && (
+                  <div className="p-1 h-5 w-5 bg-red-500 text-white rounded-[50%] flex justify-center items-center">
+                    {localCart.length}
+                  </div>
+                )}
+          </Link>
+          {session ? (
+            <>
+              <p
+                onClick={() => {
+                  signOut();
+                  localStorage.removeItem("cart");
+                }}
+              >
+                Sign out
+              </p>
+            </>
+          ) : (
+            <Link href="/signin">Sign in</Link>
+          )}
+        </div>
+        <button onClick={toggleNavbar} className="md:hidden">
+          {isOpen ? <X /> : <AlignJustify />}
+        </button>
+        {isOpen && (
+          <div className=" flex flex-col items-end basis-full w-1/2">
+            <Link href={"/cart"} className="flex">
+              <ShoppingCart />
+              <p className="pl-1">Cart</p>
+
+              {session
+                ? cart &&
+                  cart.cartItems &&
+                  cart.cartItems.length > 0 && (
+                    <div className="p-1 h-5 w-5 bg-red-500 text-white rounded-[50%] flex justify-center items-center">
+                      {cart.cartItems.length}
+                    </div>
+                  )
+                : localCart &&
+                  localCart.length > 0 && (
+                    <div className="p-1 h-5 w-5 bg-red-500 text-white rounded-[50%] flex justify-center items-center">
+                      {localCart.length}
+                    </div>
+                  )}
+            </Link>
+            {session ? (
+              <>
+                <p
+                  onClick={() => {
+                    signOut();
+                    localStorage.removeItem("cart");
+                  }}
+                >
+                  Sign out
+                </p>
+              </>
+            ) : (
+              <Link href="/signin">Sign in</Link>
+            )}
           </div>
-        </Link>
-        {session ? (
-          <>
-            <p
-              onClick={() => {
-                signOut();
-                localStorage.removeItem("cart");
-              }}
-            >
-              Sign out
-            </p>
-          </>
-        ) : (
-          <Link href="/signin">Sign in</Link>
         )}
       </div>
-    </div>
+    </header>
   );
 };
 
